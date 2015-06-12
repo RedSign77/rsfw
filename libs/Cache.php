5201c2c09b34aba3157408d6b5f6b9fe105b352d
Németh Zoltán <nemeth.zoltan@aion.hu>
2015-06-12 15:01:21 +0200

    Update caching

--- libs/Cache.php
+++ libs/Cache.php
@@ -3,7 +3,7 @@ class Cache extends Singleton
 {
 
 	private $cacheType = "Session"; // Modify and generate init{$cacheType} function
-	private $defaultTTL = "1200"; // In seconds
+	private $defaultTTL = 1200; // In seconds
 
 	/**
 	 * Construct
@@ -23,24 +23,55 @@ class Cache extends Singleton
 		}
 	}
 
+	/**
+	 * Add data to the cache
+	 *
+	 * @param      $data
+	 * @param null $key
+	 * @param null $ttl
+	 */
 	public function add($data, $key = null, $ttl = null)
 	{
 
 	}
 
-	public function get()
+	/**
+	 *  Get data from the cache
+	 *
+	 * @param string $key
+	 */
+	public function get($key)
 	{
-
+		if (method_exists($this, "get".$this->cacheType)) {
+			return $this->{"get".$this->cacheType}($key);
+		}
 	}
 
+	/**
+	 * Initialize Session caching or invalidate cache
+	 */
 	private function initSession()
 	{
-		if (!isset($_SESSION[RS_CACHE_KEY])) {
+		if (!isset($_SESSION[RS_CACHE_KEY]) || $_SESSION[RS_CACHE_KEY]['init'] < time()) {
 			$_SESSION[RS_CACHE_KEY] = array(
-				'init' => time(),
+				'init' => time() + $this->defaultTTL,
 				'data' => null,
 			);
 		}
+	}
+
+	/**
+	 * Get session cache data
+	 *
+	 * @param $key
+	 * @return bool
+	 */
+	private function getSession($key)
+	{
+		if (isset($_SESSION[RS_CACHE_KEY][$key])) {
+			return $_SESSION[RS_CACHE_KEY][$key];
+		}
+		return false;
 	}
 
 }
