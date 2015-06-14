7ac8eb58a861ab03dcc91a33e57b30ad75a02e9b
Zoltán Németh <signred@gmail.com>
2015-06-14 16:59:41 +0200

    Add Session caching

--- libs/Cache.php
+++ libs/Cache.php
@@ -1,80 +1,90 @@
 <?php
+
 /**
  * Class Cache
  */
-class Cache extends Singleton implements ICache
-{
+class Cache extends
+    Singleton implements
+    ICache {
 
-	private $cacheType = "Session"; // Modify and generate init{$cacheType} function
-	private $defaultTTL = 1200; // In seconds
+    private $cacheType  = "Session"; // Modify and generate init{$cacheType} function
+    private $defaultTTL = 1200; // In seconds
 
-	/**
-	 * Construct
-	 */
-	protected function __construct()
-	{
-		$this->initialize();
-	}
+    /**
+     * Construct
+     */
+    protected function __construct() {
+        $this->initialize();
+    }
 
-	/**
-	 * Default initialize cache
-	 */
-	private function initialize()
-	{
-		if (method_exists($this, "init".$this->cacheType)) {
-			$this->{"init".$this->cacheType}();
-		}
-	}
+    /**
+     * Default initialize cache
+     */
+    private function initialize() {
+        if (method_exists($this, "init" . $this->cacheType)) {
+            $this->{"init" . $this->cacheType}();
+        }
+    }
 
-	/**
-	 * Add data to the cache
-	 *
-	 * @param      $data
-	 * @param null $key
-	 * @param null $ttl
-	 */
-	public function add($data, $key = null, $ttl = null)
-	{
+    /**
+     * Add data to the cache
+     *
+     * @param      $data
+     * @param null $key
+     * @param null $ttl
+     */
+    public function add($data, $key = NULL, $ttl = NULL) {
+        if (method_exists($this, "add" . $this->cacheType)) {
+            return $this->{"add" . $this->cacheType}($data, $key, $ttl);
+        }
+    }
 
-	}
+    /**
+     *  Get data from the cache
+     *
+     * @param string $key
+     */
+    public function get($key) {
+        if (method_exists($this, "get" . $this->cacheType)) {
+            return $this->{"get" . $this->cacheType}($key);
+        }
+    }
 
-	/**
-	 *  Get data from the cache
-	 *
-	 * @param string $key
-	 */
-	public function get($key)
-	{
-		if (method_exists($this, "get".$this->cacheType)) {
-			return $this->{"get".$this->cacheType}($key);
-		}
-	}
+    /**
+     * Initialize Session caching or invalidate cache
+     */
+    private function initSession() {
+        if (!isset($_SESSION[RS_CACHE_KEY]) || $_SESSION[RS_CACHE_KEY]['init'] < time()) {
+            $_SESSION[RS_CACHE_KEY] = array(
+                'init' => time() + $this->defaultTTL,
+                'data' => NULL,
+            );
+        }
+    }
 
-	/**
-	 * Initialize Session caching or invalidate cache
-	 */
-	private function initSession()
-	{
-		if (!isset($_SESSION[RS_CACHE_KEY]) || $_SESSION[RS_CACHE_KEY]['init'] < time()) {
-			$_SESSION[RS_CACHE_KEY] = array(
-				'init' => time() + $this->defaultTTL,
-				'data' => null,
-			);
-		}
-	}
+    /**
+     * Get session cache data
+     *
+     * @param $key
+     *
+     * @return bool
+     */
+    private function getSession($key) {
+        if (isset($_SESSION[RS_CACHE_KEY][$key])) {
+            return $_SESSION[RS_CACHE_KEY][$key];
+        }
+        return FALSE;
+    }
 
-	/**
-	 * Get session cache data
-	 *
-	 * @param $key
-	 * @return bool
-	 */
-	private function getSession($key)
-	{
-		if (isset($_SESSION[RS_CACHE_KEY][$key])) {
-			return $_SESSION[RS_CACHE_KEY][$key];
-		}
-		return false;
-	}
+    /**
+     * Set session cache data
+     *
+     * @param $data
+     * @param $key
+     * @param $ttl
+     */
+    private function addSession($data, $key, $ttl) {
+
+    }
 
 }
