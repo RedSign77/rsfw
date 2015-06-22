d0c31143aa2b1ad93ac4aba3cba6153de4719765
Németh Zoltán <nemeth.zoltan@aion.hu>
2015-06-22 11:51:04 +0200

    Reorganize system

--- libs/Router.php
+++ libs/Router.php
@@ -9,14 +9,27 @@ class Router extends Singleton
 	private $router = array();
 	private $page = array();
 
+	/**
+	 * Construct
+	 */
 	protected function __construct() {
 		$this->readRoutingArray();
 	}
 
+	/**
+	 * Read routing array from config
+	 *
+	 * @throws Exception
+	 */
 	private function readRoutingArray() {
 		$this->router = Core::readConfigJSON("routing");
 	}
 
+	/**
+	 * Process request
+	 *
+	 * @param Request $request
+	 */
 	public function process(Request $request) {
 		if (isset($this->router[$request->getController()])) {
 			$this->page = $this->router[$request->getController()];
@@ -24,6 +37,20 @@ class Router extends Singleton
 			return;
 		}
 		$request->reset();
+	}
+
+	/**
+	 * For dump or error monitoring
+	 *
+	 * @return string
+	 */
+	public function __toString() {
+		$ret = "<pre>";
+		$ret .= "<b>" . get_class($this) . " class</b>";
+		$ret .= "<br>Page: <br>" . var_export($this->page, true);
+		//$ret .= "<br>Router array: <br>" . var_export($this->router, true);
+		$ret .= "</pre>";
+		return $ret;
 	}
 
 }
--- sys/generator.php
+++ sys/generator.php
@@ -6,46 +6,4 @@ $request = Request::getInstance();
 $cache = SessionCache::getInstance();
 $router = Router::getInstance();
 $router->process($request);
-echo $request;
-/*
-
-$_SESSION['page'] = $arrPages['kezdolap'];
-
-if (User::isLogged() && $_SESSION['page']['id'] == "login")
-    $_SESSION['page'] = $arrPages['kezdolap'];
-
-if (isset($_SESSION['REQUEST'])) {
-    $_SESSION['GURL'] = explode("/", $_SESSION['REQUEST']);
-    if (isset($arrPages[$_SESSION['GURL'][0]])) {
-        $key = $_SESSION['GURL'][0];
-        $page = $arrPages[$key];
-        if ($page['rank'] > 0 && !User::isLogged()) {
-            //$_SESSION['page'] = $arrPages['login'];
-            $_SESSION['page'] = $arrPages['kezdolap'];
-        } else {
-            if ($key == "login" && User::isLogged())
-                $_SESSION['page'] = $arrPages['kezdolap'];
-            else
-                $_SESSION['page'] = $page;
-        }
-    }
-}
-if(User::isLogged() && $_SESSION['page']['id']=="logout")
-    $user->logout();
-
-
-if (is_file("process/p_" . $_SESSION['page']['file']))
-    include 'process/p_' . $_SESSION['page']['file'];
-elseif (isset($_GET['debug']))
-    echo "<br>Warning: No default process found.";
-if (isset($_SESSION['page']['process'])) {
-    if (is_array($_SESSION['page']['process'])) {
-        foreach ($_SESSION['page']['process'] as $value) {
-            if (is_file("process/" . $value))
-                include_once "process/" . $value;
-            elseif (isset($_GET['debug']))
-                echo "<br>Warning: " . $value . " process not found.";
-        }
-    }
-}
-*/
+echo $router;
