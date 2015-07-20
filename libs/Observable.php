c63c25d8b6e64faa41cf99e5fbfabc1a825c4907
Németh Zoltán <nemeth.zoltan@aion.hu>
2015-07-20 12:55:11 +0200

    Automatic observer attach function from config

--- libs/Observable.php
+++ libs/Observable.php
@@ -8,13 +8,26 @@ abstract class Observable implements IObservable
 
 	protected $observers = array();
 
-	public function attach(IObserver $observer) {
+	protected function __construct() {
+		$this->autoAttach();
+	}
+
+	public function attach(IObserver &$observer) {
 		$this->observers[] = $observer;
 	}
 
 	public function notifyObservers($action) {
 		foreach ($this->observers as $observer) {
 			$observer->update($this, $action);
+		}
+	}
+
+	private function autoAttach() {
+		$observers = Core::readConfigJSON(get_class($this));
+		if ($observers && count($observers) > 0) {
+			foreach ($observers as $observer) {
+				$this->attach(new $observer());
+			}
 		}
 	}
 }
